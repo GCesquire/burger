236ca3d9dfab8808f055a705ca67dafd14f6a8d3
Giovanny Caquias <giovannycaquias.esq@gmail.com>
2019-04-18 12:10:15 -0400

    Seventh Commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+--Seeds file to populate database with some starter data--
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Umami Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bleecker Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mom's Burger", true);
