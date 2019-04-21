697092fbd6b9d4ca842860f404bc67d50f0909b7
Giovanny Caquias <giovannycaquias.esq@gmail.com>
2019-04-21 18:25:12 -0400

    15th commit

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,5 @@
-//Need to require connection.js so that the ORM can communicate/talk with the database.
 var connection = require("./connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
     var arr = [];
 
@@ -16,34 +10,24 @@ function printQuestionMarks(num) {
     return arr.toString();
 }
 
-// Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
     var arr = [];
 
-// loop through the keys and push the key/value as a string int arr
+
     for (var key in ob) {
         var value = ob[key];
-// check to skip hidden properties
         if (Object.hasOwnProperty.call(ob, key)) {
-// if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
                 value = "'" + value + "'";
             }
-// e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-// e.g. {sleepy: true} => ["sleepy=true"]
             arr.push(key + "=" + value);
         } 
     }
-
-// translate array of strings to a single comma-separated string
     return arr.toString();
 }
 
-//Object for all our SQL statement functions.
-//Create the methods that will execute the necessary MySQL commands in the controllers.
-//These are the methods you will need to use in order to retrieve and store data in your database.
+
 var orm = {
-//Select all function/query
     all: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function(err, result) {
@@ -54,7 +38,6 @@ var orm = {
         });
     },
 
-//Create function/query
     create: function(table, cols, vals, cb) {
         var queryString = "INSERT INTO " + table;
 
@@ -76,8 +59,6 @@ var orm = {
         });
     },
 
-//Update function/query.
-// An example of objColVals would be {name: panther, sleepy: true}
     update: function(table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
 
@@ -96,7 +77,6 @@ var orm = {
         });
     },
 
-//Delete function/query
     delete: function(table, condition, cb) {
         var queryString = "DELETE FROM " + table;
         queryString += " WHERE ";
@@ -113,5 +93,4 @@ var orm = {
 
 };
 
-//Export the orm object.
 module.exports = orm;
